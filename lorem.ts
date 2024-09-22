fb3b905a63e30b6c5eaa7a1c51cad923b6dd0465
Marc Biegota <marc.biegota@web.de>
2024-09-22 18:24:40 +0200

    better approach for concat of result string

--- lorem.ts
+++ lorem.ts
@@ -13,9 +13,9 @@
  * @module
  */
 
-const loremParagraph1 =
+const loremParagraph1: string =
     "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
-const wordsParagraph1 = loremParagraph1.split(" ");
+const wordsParagraph1: string[] = loremParagraph1.split(" ");
 
 /**
  * Creates a string with a certain number of words from the basic text “lorem ipsum”.
@@ -24,19 +24,11 @@ const wordsParagraph1 = loremParagraph1.split(" ");
  * @returns A string with the number of words from "lorem ipsum".
  */
 export default function generateWords(countOfWords: number): string {
-    let result: string = "";
-    let wordCounter: number = 0;
+    const result: string[] = [];
 
     for (let index = 0; index < countOfWords; index++) {
-        result += wordsParagraph1[wordCounter];
-        wordCounter++;
-        if (wordCounter >= wordsParagraph1.length) {
-            wordCounter = 0;
-        }
-        if (index < countOfWords - 1) {
-            result += " ";
-        }
+        result.push(wordsParagraph1[index % wordsParagraph1.length]);        
     }
 
-    return result;
+    return result.join(" ");
 }
