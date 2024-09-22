50fd0ae535501e704b718e9f9e150c9ec7286e16
Marc Biegota <marc.biegota@web.de>
2024-09-22 13:48:00 +0200

    introduce real lorem text and add more unit tests

--- lorem.ts
+++ lorem.ts
@@ -1,9 +1,16 @@
+const loremParagraph1 = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+const wordsParagraph1 = loremParagraph1.split(" ");
+
 export function words(numberOfWords: number): string {
-    let result: string = "";
-    const lorem: string = "lorem";
+    let result: string = "";    
+    let wordCounter: number = 0;
 
     for (let index = 0; index < numberOfWords; index++) {
-        result += lorem;
+        result += wordsParagraph1[wordCounter];
+        wordCounter++;
+        if (wordCounter >= wordsParagraph1.length) {
+            wordCounter = 0;
+        }
         if (index < numberOfWords - 1) {
             result += " ";
         }
--- lorem_test.ts
+++ lorem_test.ts
@@ -1,6 +1,30 @@
 import { assertEquals } from "@std/assert";
 import { words } from "./lorem.ts";
 
-Deno.test(function wordsTest() {
-    assertEquals(words(2), "lorem lorem");
-  });
+Deno.test(function zeroWordTest() {
+  assertEquals(words(0), "");
+});
+
+Deno.test(function singleWordTest() {
+  assertEquals(words(1), "Lorem");
+});
+
+Deno.test(function twoWordsTest() {
+  assertEquals(words(2), "Lorem ipsum");
+});
+
+Deno.test(function tenWordsTest() {
+  assertEquals(words(10), "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam");
+});
+
+Deno.test(function maxParagraphLengthTest() {
+  assertEquals(words(150), "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
+});
+
+Deno.test(function maxParagraphLengthPlus1Test() {
+  assertEquals(words(151), "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem");
+});
+
+Deno.test(function maxParagraphLengthPlus3Test() {
+  assertEquals(words(153), "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor");
+});
