e47a718f0e6c6e584bc9506760626ccb9dc7988a
Marc Biegota <marc.biegota@web.de>
2024-09-22 14:22:04 +0200

    remove main.ts

--- main.ts
+++
@@ -1,7 +0,0 @@
-import { words } from "./lorem.ts";
-
-
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log(words(2));
-}
